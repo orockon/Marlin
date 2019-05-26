37162765578594bf06462d551818804230e4a1c7
dot-bob <robfrommendon@gmail.com>
2019-05-26 14:51:13 -0600

    Add 0.9deg stepper to z axis.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -641,7 +641,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 75.797, 128 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 151.594, 128 } // Z 0.9 deg stepper
 
 /**
  * Default Max Feed Rate (mm/s)
